a9c06b129af6298c5f7f5919f2d56c55514411c7
developerbola <developerbola08@gmail.com>
2026-03-07 19:37:55 +0500

    callback icon loading

--- client/src/pages/Callback.tsx
+++ client/src/pages/Callback.tsx
@@ -1,10 +1,15 @@
 import { useEffect } from "react";
 import { supabase } from "@/lib/supabase";
+import { Loader2Icon } from "lucide-react";
 
 export default function AuthCallback() {
   useEffect(() => {
     supabase.auth.getSession();
   }, []);
 
-  return <p>Completing sign in...</p>;
+  return (
+    <div className="h-screen w-full grid place-items-center">
+      <Loader2Icon className="animate-spin" />
+    </div>
+  );
 }
